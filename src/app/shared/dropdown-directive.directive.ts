3c47ee9cffe28071a55de0465f68ddc133080c17
mugheeskhan1 <mugheeskhan36@gmail.com>
2022-10-26 18:37:12 +0500

    Dropdown directive functionality

--- src/app/shared/dropdown-directive.directive.ts
+++ src/app/shared/dropdown-directive.directive.ts
@@ -1,10 +1,14 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appDropdownDirective]'
 })
 export class DropdownDirective {
-
+  //class.open is the class which opens drowpdown list
+  @HostBinding('class.open') isOpen = false;
+  @HostListener('click') toggleOpen() {
+    this.isOpen = !this.isOpen;
+  }
   constructor() { }
 
 }
