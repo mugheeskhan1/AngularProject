abf16132d4c8920b0d7c13e887d87c8bccc400c6
mugheeskhan1 <mugheeskhan36@gmail.com>
2022-11-15 16:41:31 +0500

    Ingredients are now visible on selected recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,25 @@
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A test Recipe 1', 'Test 1', 'https://www.acouplecooks.com/wp-content/uploads/2021/04/Baked-Quesadilla-in-Oven-003.jpg'),
-        new Recipe('A test Recipe 2', 'Test 2', 'https://www.acouplecooks.com/wp-content/uploads/2021/04/Baked-Quesadilla-in-Oven-003.jpg'),
+        new Recipe('A test Recipe 1',
+            'What Else you need?',
+            'https://www.acouplecooks.com/wp-content/uploads/2021/04/Baked-Quesadilla-in-Oven-003.jpg',
+            [
+                new Ingredient('Meat', 23),
+                new Ingredient('French Fries', 13),
+            ]),
+        new Recipe('A test Recipe 2',
+            'What Else you need?',
+            'https://www.acouplecooks.com/wp-content/uploads/2021/04/Baked-Quesadilla-in-Oven-003.jpg',
+            [
+                new Ingredient('Buns', 40),
+                new Ingredient('Eggs', 10),
+            ]),
     ];
 
     getRecipes() {
