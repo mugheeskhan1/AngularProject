19cc4f7b615988567fd6919ea3a250c8a2891a9f
mugheeskhan1 <mugheeskhan36@gmail.com>
2022-12-17 19:45:56 +0500

    recipe's child routes added

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,18 +1,30 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
-  styleUrls: ['./recipe-detail.component.css']
+  styleUrls: ['./recipe-detail.component.css'],
 })
 export class RecipeDetailComponent implements OnInit {
-
-  @Input() recipe!: Recipe;
-  constructor(private recipeService: RecipeService) { }
+  recipe!: Recipe;
+  id!: number;
+  constructor(
+    private recipeService: RecipeService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    //method 1: will only work once
+    // const id = this.route.snapshot.params['id'];
+
+    //method 2: will work all times
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params['id'];
+      this.recipe = this.recipeService.getRecipe(this.id);
+    });
   }
 
   onAddToShoppingList() {
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,37 +1,39 @@
-import { EventEmitter, Injectable } from "@angular/core";
-import { Ingredient } from "../shared/ingredient.model";
-import { ShoppingListService } from "../shopping-list/shopping-list.service";
-import { Recipe } from "./recipe.model";
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Recipe } from './recipe.model';
 
 //for using shpping list service
 @Injectable()
 export class RecipeService {
-    recipeSelected = new EventEmitter<Recipe>();
-    constructor(private slService: ShoppingListService) { }
+  recipeSelected = new EventEmitter<Recipe>();
+  constructor(private slService: ShoppingListService) {}
 
-    private recipes: Recipe[] = [
-        new Recipe('A test Recipe 1',
-            'What Else you need?',
-            'https://www.acouplecooks.com/wp-content/uploads/2021/04/Baked-Quesadilla-in-Oven-003.jpg',
-            [
-                new Ingredient('Meat', 23),
-                new Ingredient('French Fries', 13),
-            ]),
-        new Recipe('A test Recipe 2',
-            'What Else you need?',
-            'https://www.acouplecooks.com/wp-content/uploads/2021/04/Baked-Quesadilla-in-Oven-003.jpg',
-            [
-                new Ingredient('Buns', 40),
-                new Ingredient('Eggs', 10),
-            ]),
-    ];
+  private recipes: Recipe[] = [
+    new Recipe(
+      'A test Recipe 1',
+      'What Else you need?',
+      'https://www.acouplecooks.com/wp-content/uploads/2021/04/Baked-Quesadilla-in-Oven-003.jpg',
+      [new Ingredient('Meat', 23), new Ingredient('French Fries', 13)]
+    ),
+    new Recipe(
+      'A test Recipe 2',
+      'What Else you need?',
+      'https://www.acouplecooks.com/wp-content/uploads/2021/04/Baked-Quesadilla-in-Oven-003.jpg',
+      [new Ingredient('Buns', 40), new Ingredient('Eggs', 10)]
+    ),
+  ];
 
-    getRecipes() {
-        //a copy of this array will be shared not orignal array
-        return this.recipes.slice();
-    }
+  getRecipes() {
+    //a copy of this array will be shared not orignal array
+    return this.recipes.slice();
+  }
 
-    addIngredientsToShoppingList(ingredients: Ingredient[]) {
-        this.slService.addIngredients(ingredients);
-    }
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
 }
